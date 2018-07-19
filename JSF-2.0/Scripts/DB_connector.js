8fbb7203217e25877a14f0357efe8f463baf9591
Rizzo98 <andrearizzardi98@gmail.com>
2018-07-19 16:52:17 +0200

    inizio creazione db, da finire

--- JSF-2.0/Scripts/DB_connector.js
+++ JSF-2.0/Scripts/DB_connector.js
@@ -1,18 +1,21 @@
 module.exports = function(sql){
-  //TODO creade db
+  //TODO create db
   var db = new sql.Db({
     platform: "SQLite",
-    file: "./chinook.db",
+    file: "./Database/db.db",
     schema: [
       {
-        name: "test",
-        sql: ["create table if not exists employees (id integer primary key, test1 text)"]
+        name: "default",
+        sql: [
+              "create table if not exists User (id integer primary key autoincrement, usr text not null, mail text not null, pwd text not null)",
+              "create table if not exists Combat_file (id integer primary key autoincrement, usr_id integer, txt text not null, parsed_txt text not null, foreign key(usr_id) references User(id))",
+              "create table if not exists Match_type (id integer primary key autoincrement, name text not null, n_usr integer not null)",
+              "create table if not exists Current_match (id integer primary key autoincrement, date DATE DEFAULT (datetime('now','localtime')), state text default('searching'), type integer not null, missing_users integer not null,foreign key(type) references Match_type(id))",
+              "create trigger change_missing_users after insert on Current_match begin  = (select name from Match_type where id = new.type limit 1)" //TODO finire trigger
+             ]
       }]
   });
 
-  db.query('SELECT * FROM employees',(err,rows)=>{
-    console.log(rows)
-  })
   db.close();
 
 }
